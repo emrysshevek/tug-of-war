b0026cbeb538217bafdee819ce2dc7a1243e9ffd
Emrys Shevek <emrysshevek@gmail.com>
2024-12-29 22:30:30 -0800

    mini games shuffled

--- scenes/game.gd
+++ scenes/game.gd
@@ -7,17 +7,23 @@ signal game_ended()
 
 @onready var mini_games: Array[PackedScene] = [
 	preload("res://scenes/mini_games/hold.tscn"),
-	preload("res://scenes/mini_games/dodge.tscn"),
-	preload("res://scenes/mini_games/react.tscn"),
 	preload("res://scenes/mini_games/time.tscn"),
-	preload("res://scenes/mini_games/mash.tscn")
+	preload("res://scenes/mini_games/react.tscn"),
+	preload("res://scenes/mini_games/dodge.tscn"),
 ]
 @onready var rope: Rope = $Rope
 
 var game_index := -1
+var available_games: Array[PackedScene] = []
+var prev_game: PackedScene = null
 
 func _ready() -> void:
 	Globals.phase_score = 0
+	if Globals.current_side == -1:
+		$Background/Control/ColorRect.color = Color.WHITE
+	else:
+		$Background/Control/ColorRect.color = Color.BLACK
+	available_games = mini_games.duplicate()
 	start_game()
 	
 func start_game() -> void:
@@ -29,11 +35,23 @@ func _process(delta: float) -> void:
 		rope.value = Globals.current_side * active_game.score / float(Globals.MAX_MINI_GAME_SCORE)
 
 func _next_game() -> void:
-	game_index = (game_index + 1) % len(mini_games)
-	active_game = mini_games[game_index].instantiate()
+	if len(available_games) == 0:
+		print("shuffling games")
+		available_games = mini_games.duplicate()
+		available_games.shuffle()
+
+	if available_games[0] == prev_game:
+		print("avoiding duplicate game")
+		prev_game = available_games[-1]
+		active_game = available_games.pop_back().instantiate()
+	else:
+		prev_game = available_games[0]
+		active_game = available_games.pop_front().instantiate()
+	
 	active_game.difficulty = difficulty
 	active_game.won.connect(_on_mini_game_won)
 	active_game.lost.connect(_on_mini_game_lost)
+
 	add_child(active_game)
 	print("playing next mini-game: ", active_game.name)
 
--- scenes/mini_games/mini_game.gd
+++ scenes/mini_games/mini_game.gd
@@ -4,11 +4,15 @@ signal won()
 signal lost()
 
 @export var difficulty = 1
+var base_pull_speed := 10.0
+var pull_speed: float
 
 var score = 0.0
 
 func _ready() -> void:
 	Globals.inversion_amount = 0
+	difficulty = Globals.phase + 1
+	pull_speed = base_pull_speed * difficulty
 
 func _process(_delta: float) -> void:
 	score = clamp(score, -Globals.MAX_MINI_GAME_SCORE, Globals.MAX_MINI_GAME_SCORE)
